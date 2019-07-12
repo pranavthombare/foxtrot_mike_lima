4b35b9d343fe6a45e823cc0a5357417aed5a3fd6
Pranav Thombare <pranavthombare97@gmail.com>
2019-07-12 15:41:58 +0100

    Initial commit with some trial code

---
+++ foxtrot_mike_lima.py
@@ -0,0 +1,21 @@
+import csv
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+
+#Sorting images from links in CSV
+
+filename = "data/test"
+
+contents = []
+with open('data/test.csv','r') as csvf: # Open file in read mode
+    urls = csv.reader(csvf)
+    for url in urls:
+        contents.append(url) # Add each url to list contents
+
+for url in contents:  # Parse through each url in the list.
+    page = urlopen(url[0]).read()
+    soup = BeautifulSoup(page, "html.parser")
+print(soup)
